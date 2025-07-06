2980116c5fa1cd43e47aa60b2b5d0bf21c29da81
Hosny Ashraf <hosny9938@gmail.com>
2025-07-06 07:41:11 +0300

    ..

---
+++ CINEMA/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,76 @@
+﻿using CINEMA.Data;
+using CINEMA.Model_view;
+using CINEMA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CINEMA.Areas.Admin.Controllers
+{ [Area("Admin")]
+    public class MovieController : Controller
+    {
+        private ApplicationDbContext _context = new();
+        public IActionResult Index()
+        {
+            var Movies = _context.Movies.Include(e => e.Category);
+            return View(Movies.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var Categories = _context.Categories;
+            var Cinemas = _context.Cinemas;
+
+            CategoryWithCinemaVM categoryWithCinemaVM = new()
+            {
+                Categories = Categories.ToList(),
+                Cinemas =Cinemas.ToList()
+            };           
+            return View(categoryWithCinemaVM);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie Movie)
+        { 
+            _context.Add(Movie);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edite([FromRoute]int Id)
+        { 
+        var Movie = _context.Movies.Find(Id);
+            var Categories = _context.Categories;
+            var Cinemas = _context.Cinemas;
+
+            CategoryWithCinemaVM categoryWithCinemaVM = new()
+            {
+                Categories = Categories.ToList(),
+                Cinemas = Cinemas.ToList(),
+                Movie = Movie
+            };
+            return View(categoryWithCinemaVM);
+        }
+
+        [HttpPost]
+        public IActionResult Edite(Movie Movie)
+        {
+            _context.Update(Movie);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var Movie = _context.Movies.Include(e => e.Cinema).Include(e => e.Category).FirstOrDefault(e => e.Id == id);
+           
+               
+                return View(Movie);
+            
+        }
+      
+    }
+
+}
---
+++ CINEMA/Model view/CategoryWithCinemaVM.cs
@@ -0,0 +1,11 @@
+﻿using CINEMA.Models;
+
+namespace CINEMA.Model_view
+{
+    public class CategoryWithCinemaVM
+    {
+        public List<Category> Categories { get; set; }
+        public List<Cinema> Cinemas{ get; set; }
+        public Movie? Movie { get; set; }
+    }
+}
